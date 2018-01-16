e866a30b19f950e59164fb7cd9f67ad5e5915fd1
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:26:31 +0530

    Modified GraphQLService class

--- src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
@@ -4,11 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+
 @Service
 public class GraphQLService {
 	
 	@Value("classpath:books.graphql")
 	Resource resource;
 	
-	
+	@PostConstruct
+	public void loadSchema() throws IOException {
+		File schemaFile = resource.getFile();
+	}
 }
