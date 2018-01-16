5a638401d43e78f2100ba4cb33db27b30f92b259
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 14:04:03 +0530

    Added getGraphQL

--- src/main/java/io/yadnyesh/sbgraphql/controller/BookController.java
+++ src/main/java/io/yadnyesh/sbgraphql/controller/BookController.java
@@ -1,7 +1,10 @@
 package io.yadnyesh.sbgraphql.controller;
 
+import graphql.ExecutionResult;
 import io.yadnyesh.sbgraphql.service.GraphQLService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,8 @@ public class BookController {
 	GraphQLService graphQLService;
 	
 	@PostMapping
-	public void getAllBooks(@RequestBody String query) {
-		graphQLService.getGraphQL().execute(query);
+	public ResponseEntity<Object> getAllBooks(@RequestBody String query) {
+		ExecutionResult executionResult = graphQLService.getGraphQL().execute(query);
+		return new ResponseEntity<>(executionResult, HttpStatus.OK);
 	}
 }
