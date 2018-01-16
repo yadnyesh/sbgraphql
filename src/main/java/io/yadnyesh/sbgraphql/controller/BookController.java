a0cdd4f7fa45bc9605e4ce45eb35d15787c052f3
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:18:53 +0530

    Adding getAllBooks

--- src/main/java/io/yadnyesh/sbgraphql/controller/BookController.java
+++ src/main/java/io/yadnyesh/sbgraphql/controller/BookController.java
@@ -1,5 +1,6 @@
 package io.yadnyesh.sbgraphql.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/books")
 @RestController
 public class BookController {
-
+	
+	@PostMapping
+	public void getAllBooks(@RequestBody String query) {
+	
+	}
 }
