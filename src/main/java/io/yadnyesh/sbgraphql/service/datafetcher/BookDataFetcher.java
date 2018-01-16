0d1263f8284b053d2ca4297bf9641b62dd091bec
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:49:55 +0530

    Created file BookDataFetcher

--- src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/BookDataFetcher.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/BookDataFetcher.java
@@ -1,3 +1,15 @@
 package io.yadnyesh.sbgraphql.service.datafetcher;
 
-public class BookDataFetcher {}
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import io.yadnyesh.sbgraphql.model.Book;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDataFetcher implements DataFetcher<Book>{
+	
+	@Override
+	public Book get(DataFetchingEnvironment dataFetchingEnvironment) {
+		return null;
+	}
+}
