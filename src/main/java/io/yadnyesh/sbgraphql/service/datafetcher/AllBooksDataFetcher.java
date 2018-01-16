cdfc1d127316ab9067a6e0db0acb6829436d4c0f
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:51:37 +0530

    Modified AllBookDataFetcher

--- src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/AllBooksDataFetcher.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/AllBooksDataFetcher.java
@@ -1,6 +1,17 @@
 package io.yadnyesh.sbgraphql.service.datafetcher;
 
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import io.yadnyesh.sbgraphql.model.Book;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class AllBooksDataFetcher {}
+public class AllBooksDataFetcher implements DataFetcher<List<Book>>{
+	
+	@Override
+	public List<Book> get(DataFetchingEnvironment dataFetchingEnvironment) {
+		return null;
+	}
+}
