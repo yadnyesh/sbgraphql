ea73eb306762432d3a7902b3bb68d67d3f7354a7
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:48:40 +0530

    Created file AllBooksDataFetcher

--- src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
@@ -7,6 +7,7 @@ import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import io.yadnyesh.sbgraphql.service.datafetcher.AllBooksDataFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
---
+++ src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/AllBooksDataFetcher.java
@@ -0,0 +1,3 @@
+package io.yadnyesh.sbgraphql.service.datafetcher;
+
+public class AllBooksDataFetcher {}
