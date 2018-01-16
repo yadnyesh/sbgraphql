44a3cff8d8801c69aabd8499a3b9decb6196d2bf
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:16:42 +0530

    Started with RestController

---
+++ src/main/java/io/yadnyesh/sbgraphql/controller/BookController.java
@@ -0,0 +1,11 @@
+package io.yadnyesh.sbgraphql.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest/books")
+@RestController
+public class BookController {
+
+}
