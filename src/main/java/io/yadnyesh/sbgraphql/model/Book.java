c92c2c992038d4341cfc87d5c1dbda8fc603427b
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:21:11 +0530

    Added LomBok annotations

--- src/main/java/io/yadnyesh/sbgraphql/model/Book.java
+++ src/main/java/io/yadnyesh/sbgraphql/model/Book.java
@@ -1,11 +1,20 @@
 package io.yadnyesh.sbgraphql.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Book {
 
 	@Id
