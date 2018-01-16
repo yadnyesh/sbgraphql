44bc2d3f6a34ee39d542e5a8b4dec77c8d2f155c
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:04:53 +0530

    First Commit when starting Spring Boot GraphQL

---
+++ src/main/java/io/yadnyesh/sbgraphql/SbgraphqlApplication.java
@@ -0,0 +1,12 @@
+package io.yadnyesh.sbgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbgraphqlApplication.class, args);
+	}
+}
