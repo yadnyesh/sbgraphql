aaeb69b1676bdffaec9985ddcba6fe23a9322a0d
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 10:06:28 +0530

    Fetch exactly one Book

--- src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/BookDataFetcher.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/BookDataFetcher.java
@@ -3,13 +3,19 @@ package io.yadnyesh.sbgraphql.service.datafetcher;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import io.yadnyesh.sbgraphql.model.Book;
+import io.yadnyesh.sbgraphql.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookDataFetcher implements DataFetcher<Book>{
 	
+	@Autowired
+	BookRepository bookRepository;
+	
 	@Override
 	public Book get(DataFetchingEnvironment dataFetchingEnvironment) {
-		return null;
+		String isn = dataFetchingEnvironment.getArgument("id");
+		return bookRepository.findOne(isn);
 	}
 }
