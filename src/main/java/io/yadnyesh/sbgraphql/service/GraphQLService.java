53e6960bdd1b90d61d2181b7ec855fbfb35864e6
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:25:18 +0530

    Created GraphQLService class

--- src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
@@ -1,3 +1,14 @@
 package io.yadnyesh.sbgraphql.service;
 
-public class GraphQLService {}
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GraphQLService {
+	
+	@Value("classpath:books.graphql")
+	Resource resource;
+	
+	
+}
