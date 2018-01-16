cb366123de31c2b059570decf5b56896fbc95f85
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:31:39 +0530

    Modified GraphQLService class

--- src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/GraphQLService.java
@@ -1,5 +1,11 @@
 package io.yadnyesh.sbgraphql.service;
 
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -17,5 +23,13 @@ public class GraphQLService {
 	@PostConstruct
 	public void loadSchema() throws IOException {
 		File schemaFile = resource.getFile();
+		TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schemaFile);
+		RuntimeWiring wiring = buildRuntimeWiring();
+		GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeRegistry);
+		graphQL = GraphQL.newGraphQL(schema).build();
+	}
+	
+	private RuntimeWiring buildRuntimeWiring() {
+		return null;
 	}
 }
