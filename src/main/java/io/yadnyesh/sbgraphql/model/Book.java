7c9722e4d8865400c5bf6a6dd34441730386bc32
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 07:20:31 +0530

    Added attributes to the model

--- src/main/java/io/yadnyesh/sbgraphql/model/Book.java
+++ src/main/java/io/yadnyesh/sbgraphql/model/Book.java
@@ -1,11 +1,21 @@
 package io.yadnyesh.sbgraphql.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
 @Entity
 public class Book {
 
-
+	@Id
+	private String isn;
+	
+	private String title;
+	
+	private String publisher;
+	
+	private String[] authors;
+	
+	private String publishDate;
 }
