c6a5b37a478bef7f0b0286a231eb94fee799a2db
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 10:18:57 +0530

    Autowire GraphQL

--- src/main/java/io/yadnyesh/sbgraphql/controller/BookController.java
+++ src/main/java/io/yadnyesh/sbgraphql/controller/BookController.java
@@ -1,5 +1,7 @@
 package io.yadnyesh.sbgraphql.controller;
 
+import io.yadnyesh.sbgraphql.service.GraphQLService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/books")
 @RestController
 public class BookController {
+	
+	@Autowired
+	GraphQLService graphQLService;
 	
 	@PostMapping
 	public void getAllBooks(@RequestBody String query) {
