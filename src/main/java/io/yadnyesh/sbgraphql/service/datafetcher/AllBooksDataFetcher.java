c9d5d64ce505d92cf1e16b47da7eaccdffb0d4f2
YB <yadnyesh.juvekar@gmail.com>
2018-01-16 10:04:41 +0530

    Inject BookRepository

--- src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/AllBooksDataFetcher.java
+++ src/main/java/io/yadnyesh/sbgraphql/service/datafetcher/AllBooksDataFetcher.java
@@ -3,12 +3,17 @@ package io.yadnyesh.sbgraphql.service.datafetcher;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import io.yadnyesh.sbgraphql.model.Book;
+import io.yadnyesh.sbgraphql.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class AllBooksDataFetcher implements DataFetcher<List<Book>>{
+	
+	@Autowired
+	BookRepository bookRepository;
 	
 	@Override
 	public List<Book> get(DataFetchingEnvironment dataFetchingEnvironment) {
